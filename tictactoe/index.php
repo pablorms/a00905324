304390f1126a336802fe81abcfcdf4810612510e
Pablo Ramos <pablo.rms@gmail.com>
2016-01-10 13:06:58 -0800

    Lab 1 - first commit

--- tictactoe/index.php
+++ tictactoe/index.php
@@ -11,17 +11,25 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+            
             $name = 'Pablo';
             $what = 'geek';
             $level = 10;
             echo 'Hi, my name is '.$name,'. and I am a level '.$level.'
             '.$what;
             
-            $hoursworked = 10;
-            $rate = 12;
-            $total = $hoursworked * $rate;
             echo '<br/>';
-            echo 'You owe me '.$total;
+            
+            $hoursworked=41;
+            $rate = 30;
+            
+            if ($hoursworked > 40) {
+            $total = $hoursworked * $rate * 1.5;
+            } else {
+            $total = $hoursworked * $rate;
+            }
+            
+            echo ($total > 0) ? 'You owe me '.$total : "You're welcome";
         ?>
     </body>
 </html>

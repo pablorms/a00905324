362fc0a6f601508d951c7071cc5193e8dde09f35
Pablo Ramos <pablo.rms@gmail.com>
2016-01-10 14:20:42 -0800

    Winner function

--- tictactoe/index.php
+++ tictactoe/index.php
@@ -11,76 +11,48 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+            if (isset($_GET['board']))    
+            {
+                $position = $_GET['board'];
+                $squares = str_split($position);
 
-            
-            $name = 'Pablo';
-            $what = 'geek';
-            $level = 10;
-            echo 'Hi, my name is '.$name,'. and I am a level '.$level.'
-            '.$what;
-            
-            echo '<br/>';
-            
-            $hoursworked=$_GET['hours'];
-            $rate = 30;
-            
-            if ($hoursworked > 40) {
-            $total = $hoursworked * $rate * 1.5;
-            } else {
-            $total = $hoursworked * $rate;
+                if (winner('x',$squares)) echo 'You win.';
+                else if (winner('o',$squares)) echo 'I win.';
+                else echo 'No winner yet.';
             }
-            
-            echo ($total > 0) ? 'You owe me '.$total : "You're welcome";
+            else
+            {
+                echo 'No game info available';
+            }
+                
         ?>
     </body>
 </html>
 <?php
 
-    // Function to determin if player won the game
+    // Function to determine if there is a winner
     function winner($token,$position) {
         $won = false;
-        if (($position[0] == $token) &&
-            ($position[1] == $token) &&
-            ($position[2] == $token)) {
-
-            $won = true;
-        } else if (($position[3] == $token) &&
-                   ($position[4] == $token) &&
-                   ($position[5] == $token)) {
-
-            $won = true;
-        } else if (($position[6] == $token) &&
-                   ($position[7] == $token) &&
-                   ($position[8] == $token)) {
-
-            $won = true;
-        } else if (($position[0] == $token) &&
-                   ($position[3] == $token) &&
-                   ($position[6] == $token)) {
-
-            $won = true;
-        } else if (($position[1] == $token) &&
-                   ($position[4] == $token) &&
-                   ($position[7] == $token)) {
-
-            $won = true;
-        } else if (($position[2] == $token) &&
-                   ($position[5] == $token) &&
-                   ($position[8] == $token)) {
-
-            $won = true;
-        } else if (($position[0] == $token) &&
-                   ($position[4] == $token) &&
-                   ($position[8] == $token)) {
-
-            $won = true;
-        } else if (($position[2] == $token) &&
-                   ($position[4] == $token) &&
-                   ($position[6] == $token)) {
-
+        
+        // test for rows
+        for($row=0; $row<9; $row+=3){
+            if (($position[$row] == $token) && ($position[1+$row]
+                == $token) && ($position[2+$row] == $token)) $won = true;
+        }
+        
+        // test for columns
+        for($col=0; $col<3; $col++){
+            if (($position[0+$col] == $token) && ($position[3+$col]
+                == $token) && ($position[6+$col] == $token)) $won = true;            
+        }
+        
+        // test for diagonals 
+        if (($position[4] == $token) &&
+            (($position[0] == $token && $position[8] == $token) ||
+             ($position[2] == $token && $position[6] == $token))) {
             $won = true;
         }
-
+        
         return $won;
     }
 ?>

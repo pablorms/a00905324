b65c83457c0b5bc433ed72f83afa661ca7cf7a95
Pablo Ramos <pablo.rms@gmail.com>
2016-01-10 13:32:12 -0800

    Lab 1 - first commit

--- tictactoe/index.php
+++ tictactoe/index.php
@@ -11,6 +11,60 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+            function winner($token,$position) {
+                $won = false;
+                if (($position[0] == $token) &&
+                    ($position[1] == $token) &&
+                    ($position[2] == $token)) {
+                    
+                    $won = true;
+                    
+                } else if (($position[3] == $token) &&
+                           ($position[4] == $token) &&
+                           ($position[5] == $token)) {
+                    
+                    $won = true;
+                    
+                } else if (($position[6] == $token) &&
+                           ($position[7] == $token) &&
+                           ($position[8] == $token)) {
+                    
+                    $won = true;
+                    
+                } else if (($position[0] == $token) &&
+                           ($position[3] == $token) &&
+                           ($position[6] == $token)) {
+                    
+                    $won = true;
+                    
+                } else if (($position[1] == $token) &&
+                           ($position[4] == $token) &&
+                           ($position[7] == $token)) {
+                    
+                    $won = true;
+                    
+                } else if (($position[2] == $token) &&
+                           ($position[5] == $token) &&
+                           ($position[8] == $token)) {
+                    
+                    $won = true;
+                    
+                } else if (($position[0] == $token) &&
+                           ($position[4] == $token) &&
+                           ($position[8] == $token)) {
+                    
+                    $won = true;
+                    
+                } else if (($position[2] == $token) &&
+                           ($position[4] == $token) &&
+                           ($position[6] == $token)) {
+                    
+                    $won = true;
+                    
+                }
+                
+                return $won;
+            }
             
             $name = 'Pablo';
             $what = 'geek';
